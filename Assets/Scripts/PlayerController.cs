9d5bb1c948afd03875a0436fb9d8cb22087d1f6b
modniczha345 <149092694+modniczha345@users.noreply.github.com>
2024-03-05 10:11:39 +0300

    1 task completed

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
+    private Vector3 _moveVector;
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+ void Update()
+{
+    // Движение
+    _moveVector = Vector3.zero;
+    if (Input.GetKey(KeyCode.W))
+        _moveVector += transform.forward;
+    if (Input.GetKey(KeyCode.S))
+        _moveVector -= transform.forward;
+    if (Input.GetKey(KeyCode.D))
+        _moveVector += transform.right;
+    if (Input.GetKey(KeyCode.A))
+        _moveVector -= transform.right;
+
+    // Нормализуем вектор движения, чтобы скорость была одинаковой во всех направлениях
+    if (_moveVector != Vector3.zero)
+        _moveVector = _moveVector.normalized;
+
+    if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+    {
+        _fallVelocity = -jumpForce;
+    }
+}
+
+
+    void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
