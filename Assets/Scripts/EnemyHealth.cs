d27f6e5f7390293f1ec32040344dc19a44d6c5c7
modniczha345 <149092694+modniczha345@users.noreply.github.com>
2024-03-08 02:25:03 +0300

    task 2.0

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float health = 100; // Добавили поле health
+    public float damage = 10; // Уменьшили урон, чтобы не убивать врага с одного удара
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();
+    }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(damage); // Вызываем метод TakeDamage()
+        }
+    }
+
+    public void TakeDamage(float damage) // Добавили метод TakeDamage()
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CancelInvoke("DestroyFireball");
+        DestroyFireball();
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+}
